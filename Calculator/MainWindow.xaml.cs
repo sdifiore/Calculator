b82528e8162db5840542aa36ba20c7e74d5ea065
sdifiore <difiores@outlook.com>
2022-07-19 12:33:27 -0300

    Remove if's

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Calculator
 {
@@ -44,30 +45,29 @@ namespace Calculator
             resultLabel.Content = "0";
         }
 
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+                result = 0;
+            }
+
+            if (sender == plusButton)
+                lastNumber += result;
+            if (sender == minusButton)
+                lastNumber -= result;
+            if (sender == divisionButton)
+                lastNumber /= result;
+            if (sender == timesButton)
+                lastNumber *= result;
+
+            resultLabel.Content = lastNumber.ToString();
+        }
+
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            int selectedValue = 0;
-
-            if (sender == zeroButton)
-                selectedValue = 0;
-            if (sender == oneButton)
-                selectedValue = 1;
-            if (sender == twoButton)
-                selectedValue = 2;
-            if (sender == threeButton)
-                selectedValue = 3;
-            if (sender == fourButton)
-                selectedValue = 4;
-            if (sender == fiveButton)
-                selectedValue = 5;
-            if (sender == sixButton)
-                selectedValue = 6;
-            if (sender == sevenButton)
-                selectedValue = 7;
-            if (sender == eightButton)
-                selectedValue = 8;
-            if (sender == nineButton)
-                selectedValue = 9;
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
 
             if (resultLabel.Content.ToString() == "0")
             {
